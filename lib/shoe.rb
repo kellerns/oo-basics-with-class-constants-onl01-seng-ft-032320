9f5fdcba4bc0c7d8e851c336da773d48ac319758
Nicholas Keller <nic.keller328@gmail.com>
2020-04-02 19:08:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -15,7 +15,9 @@ class Shoe
 
   def brand=(brand)
     @brand = brand
-    if BRANDS.include?
-    
-    BRANDS << brand
+    if BRANDS.include?(brand)
+    else
+      BRANDS << brand
+    end
+  end
 end
